a24e3528a7b91d7dff1bb86202fbd23a9797e250
moohoozaa <paramate.php@gmai.com>
2019-10-17 22:49:50 +0700

    updating our app with a shop page, collection preview component and collection item component

---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,17 @@
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToAdd.id
+  );
+
+  // add exist item
+  if (existingCartItem) {
+    return cartItems.map(cartItem =>
+      cartItem.id === cartItemToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+
+  // add new item
+  return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
